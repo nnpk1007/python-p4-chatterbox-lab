d1b6a6d79ad60915efe971b634250dcb77db2d47
Ngoc Nguyen <nnpk0410@gmail.com>
2023-09-07 07:55:20 -0500

    lab completed

--- server/app.py
+++ server/app.py
@@ -5,22 +5,77 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
+#app.run(debug=True)
 
 CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+# GET /messages: returns an array of all messages as JSON, ordered by created_at in ascending order.
+# POST /messages: creates a new message with a body and username from params, and returns the newly created post as JSON.
+
+
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = Message.query.all()
+        messages_serialized = [message.to_dict() for message in messages]
 
-@app.route('/messages/<int:id>')
+        response = make_response(messages_serialized, 200)
+
+        return response
+
+    elif request.method == "POST":
+        data = request.get_json()
+
+        new_message = Message(body=data["body"], username=data["username"])
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(message_dict, 201)
+
+        return response
+
+
+# PATCH /messages/<int:id>: updates the body of the message using params, and returns the updated message as JSON.
+# DELETE /messages/<int:id>: deletes the message from the database.
+
+
+@app.route("/messages/<int:id>", methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
 
-if __name__ == '__main__':
+    if request.method == "PATCH":
+        data = request.get_json()
+        print(data)
+
+        message.body = data["body"]
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(message_dict, 200)
+        return response
+
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {"delete_successful": True, "message": "Message deleted."}
+
+        response = make_response(response_body, 200)
+
+        return response
+
+
+if __name__ == "__main__":
     app.run(port=5555)
